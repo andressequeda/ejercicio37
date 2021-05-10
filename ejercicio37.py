1e86c29b93d929b1ecbe561a2f59f413252bf94d
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-10 09:59:11 -0500

    Add files via upload

---
+++ ejercicio37.py
@@ -0,0 +1,9 @@
+numero1  =  int ( input ( "Ingrese el primer número:" ))
+numero2  =  int ( input ( "Ingrese el segundo número:" ))
+numero3  =  int ( input ( "Ingrese el tercer número:" ))
+si  numero1  <  numero2  <  numero3 :
+    print ( "Los números se incrementan." )
+elif  numero1  >  numero2  >  numero3 :
+    print ( "Los números disminuyen." )
+otra cosa :
+    print ( "Los números no se incrementan ni se disminuyen" )
